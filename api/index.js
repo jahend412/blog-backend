2ec29cac79e162fc420374a02122bc881aa755d2
Josh Endemann <jahend44@gmail.com>
2024-01-28 18:08:12 -0600

    Added Cors
    Added CORS to fix Cors bug

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
+// Create an instance of Express
 const app = express();
+
+// Use cors middleware
+app.use(cors());
 
 app.post('/register', (req, res) => {
     res.json('test ok');
