7e37671dc4cfa82a44000b800d42a32335c3a42f
Josh Endemann <jahend44@gmail.com>
2024-01-30 19:46:54 -0600

    Added bcrypt
    Added bcrypt to hide the password within the database.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const app = express();
+const bcrypt = require('brcypt');
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -10,11 +13,16 @@ app.use(express.json());
 mongoose.connect('mongodb+srv://JoshEnd:S2WVkRDVFPt1SlZo@cluster0.wqrxsmn.mongodb.net');
 
 app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
+    const {
+        username, password } = req.body;
     try {
-        const userDoc = await User.create({ username, password });
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt),
+        });
         res.json(userDoc);
     } catch (e) {
+        console.log(e);
         res.status(400).json(e);
     }
 
