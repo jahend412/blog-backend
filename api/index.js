6a6e2cf043e6748a5a6d034cb9ab98ccc6b30f06
Josh Endemann <jahend44@gmail.com>
2024-01-31 19:33:31 -0600

    Update index.js
    Added cookie-parser, and also added a profile and logout endpoints

--- api/index.js
+++ api/index.js
@@ -5,18 +5,19 @@ const User = require('./models/User');
 const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
-const secret = 'jalksdfjlkasjdfl;kda';
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://JoshEnd:S2WVkRDVFPt1SlZo@cluster0.wqrxsmn.mongodb.net');
 
 app.post('/register', async (req, res) => {
-    const {
-        username, password } = req.body;
+    const { username, password } = req.body;
     try {
         const userDoc = await User.create({
             username,
@@ -31,17 +32,32 @@ app.post('/register', async (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const userDoc = await User.find({ username });
+    const userDoc = await User.findOne({ username });
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
         // logged in
         jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('token', token).json('ok');
+            res.cookie('token', token).json({
+                id: userDoc._id,
+                username,
+            });
         });
     } else {
-        res.status(400).json('Incorrect Password');
+        res.status(400).json('wrong credentials');
     }
-})
+});
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+});
 
 app.listen(4000);
